d5edc2d27118b1e5e159675e51981e1184e1972a
kingster121 <48852120+kingster121@users.noreply.github.com>
2025-08-12 13:09:12 +0800

    Added Vitest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
   ],
+  test: {
+    globals: true, // If you want global test functions like 'describe', 'it', etc.
+    environment: 'jsdom', // Use jsdom environment for DOM-related tests
+    setupFiles: ['./test/setup.js'], // Optional: add setup files like mock setups
+  },
 })
