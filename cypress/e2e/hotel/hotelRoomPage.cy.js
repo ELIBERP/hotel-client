28ebfb1f5f30f22c9287384cc4c808c3e35656bc
kingster121 <48852120+kingster121@users.noreply.github.com>
2025-08-14 04:06:36 +0800

    Added E2E Test Feature 3

--- cypress/e2e/hotel/hotelRoomPage.cy.js
+++ cypress/e2e/hotel/hotelRoomPage.cy.js
@@ -1,4 +1,4 @@
-describe('HotelDetails - Map Modal', () => {
+describe('Feature 3 - E2E', () => {
   // Use provided hotel ID and query params
   const hotelId = 'dwHs';
   const params = [
@@ -15,25 +15,13 @@ describe('HotelDetails - Map Modal', () => {
 
   beforeEach(() => {
     cy.visit(`http://localhost:5173/hotels/${hotelId}?${params}`);
-  });
+    
+    cy.intercept('GET', '**/hotels/*').as('hotel');
+    cy.intercept('GET', '**/hotels/*/prices*').as('prices');
 
-  it('shows map modal when address button is clicked', () => {
-    // Wait for hotel address button to appear
-    cy.contains('button', /^📍 /)
-      .should('be.visible')
-      .click();
+    // cy.visit('http://localhost:5173/hotels/48Pm?destination_id=RsBU&checkin=2025-08-26&checkout=2025-08-29&lang=en_US&currency=SGD&guests=2&partner_id=1089&landing_page=wl-acme-earn&product_type=earn');
 
-    // Modal should appear with "Hotel Location" heading
-    cy.contains('h2', 'Hotel Location').should('be.visible');
-
-    // Map should be rendered (look for the Map container)
-    cy.get('.rounded-xl').find('iframe, div').should('exist');
-
-    // Close the modal
-    cy.contains('button', 'Close').click();
-
-    // Modal should disappear
-    cy.contains('Hotel Location').should('not.exist');
+    cy.wait(['@hotel', '@prices']); // replaces cy.wait(10000)  });
   });
 
   it('shows description modal when "View more details..." button is clicked', () => {
@@ -46,12 +34,32 @@ describe('HotelDetails - Map Modal', () => {
     cy.contains('h2', 'Full Description').should('be.visible');
 
     // Modal should contain some description text
-    cy.get('.text-sm text-[#0e151b] leading-relaxed whitespace-pre-line').should('be.visible');
+    cy.get('p.text-sm.text-\\[\\#0e151b\\].leading-relaxed.whitespace-pre-line');
 
     // Close the modal
     cy.contains('button', 'Close').click();
 
     // Modal should disappear
     cy.contains('Full Description').should('not.exist');
+  });
+
+  it('views room details and clicks book now', () => {
+    // Price API takes awhile to respond
+    // cy.wait(10000);
+
+    // Click the "View Room Details" button
+    cy.get('.p-4 > .px-4')
+      .click();
+
+    // Check if the "Breakfast: Not included" paragraph is visible inside the modal/container
+    cy.contains('p', 'Breakfast: Not included').should('be.visible');
+
+    // Find and click the "Book Now" button
+    cy.contains('button', 'Book Now')
+      .should('be.visible')
+      .click();
+
+    // Add an assertion here to confirm the action, e.g., URL change or a new element appearing
+    // cy.url().should('include', 'booking-confirmation');
   });
 });
