6fa7c3ff3d91ff60326633cd8ab3a791a7066248
ELIBERP <elizz2beeth@gmail.com>
2025-08-14 20:43:58 +0800

    add backend to whitelist

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   server: {
     // Add headers to handle Content Security Policy (CSP)
     headers: {
-      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://*.gstatic.com https://cdn.tailwindcss.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.tailwindcss.com; img-src 'self' data: https://*.googleapis.com https://*.gstatic.com https://*.ggpht.com https://lh3.googleusercontent.com https://*.cloudfront.net https://*.travelapi.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' http://localhost:3000 https://*.googleapis.com https://*.gstatic.com;",
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://*.gstatic.com https://cdn.tailwindcss.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.tailwindcss.com; img-src 'self' data: https://*.googleapis.com https://*.gstatic.com https://*.ggpht.com https://lh3.googleusercontent.com https://*.cloudfront.net https://*.travelapi.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' http://localhost:3000 https://*.googleapis.com https://*.gstatic.com https://hotel-backend-cl4j.onrender.com;",
     },
   },
 });
